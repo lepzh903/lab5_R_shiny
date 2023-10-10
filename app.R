fe206f4ae7533fa4c21c6676f37f38f6ff0881d3
William <william.wijgard@gmail.com>
2023-10-10 19:44:30 +0200

    Added server function to the shiny app.

--- app.R
+++ app.R
@@ -26,3 +26,34 @@ ui <- fluidPage(
   )
 )
 
+
+server <- function(input, output) {
+  output$linePlot <- renderPlot({
+    selected_lines <- input$lines
+    period_min <- input$period_range[1]
+    period_max <- input$period_range[2]
+    
+    gg <- ggplot(data = subset(plot_data, period >= period_min & period <= period_max), aes(x = period))
+    i <- 1
+    for (line in selected_lines) {
+      gg <- gg + geom_line(aes_string(y = line, color = factor(names(plot_data[-1])[i])), linewidth = 1.5)
+      i <- i + 1
+    }
+    gg + labs(x = "Year", y = "Value (%)", color = NULL) +
+      scale_color_manual(values = c('#1F78B4', '#2D452C', '#FF7F00', '#6A3D9A', '#E41A1C')
+                         , labels = selected_lines) +
+      scale_x_continuous(breaks = seq(min(plot_data$period), max(plot_data$period), by = 2),
+                         labels = seq(min(plot_data$period), max(plot_data$period), by = 2)) +
+      theme(legend.position = "bottom", 
+            legend.text = element_text(size = 14),
+            axis.title.x = element_text(size = 16),
+            axis.title.y = element_text(size = 16),
+            axis.text.x = element_text(size = 14),
+            axis.text.y = element_text(size = 14))  
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+
+
